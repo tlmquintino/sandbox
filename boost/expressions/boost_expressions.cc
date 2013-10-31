04f9c662686327081150bd045c7ab08611d4ee84
Tiago Quintino <tiago.quintino@ecmwf.int>
2013-10-31 21:25:40 +0000

    add prod

--- boost/expressions/boost_expressions.cc
+++ boost/expressions/boost_expressions.cc
@@ -9,6 +9,8 @@
 #include <boost/noncopyable.hpp>
 #include <boost/shared_ptr.hpp>
 
+namespace maths {
+
 //-----------------------------------------------------------------------------
 
 typedef double scalar_t;
@@ -73,13 +75,16 @@ public:
 
     scalar_t value() const { return v_; }
 
-    virtual std::ostream& print( std::ostream& o ) const { return o << v_; }
+    /// returns a reference to the scalar
+    scalar_t& ref_value() { return v_; }
 
-    static VarPtr make( const scalar_t& v ) { return VarPtr( new Scalar(v) ); }
+    virtual std::ostream& print( std::ostream& o ) const { return o << v_; }
 
 protected:
     scalar_t v_;
 };
+
+VarPtr scalar( const scalar_t& s  ) { return VarPtr( new Scalar(s) ); }
 
 //-----------------------------------------------------------------------------
 
@@ -92,6 +97,8 @@ public:
     Vector( const size_t& s, const scalar_t& v = scalar_t() ) : v_(s,v) {}
     Vector( const storage_t& v ) : v_(v) {}
 
+//    virtual ~Vector() { std::cout << "deleting Vector" << std::endl; }
+
     virtual size_t size() const { return v_.size(); }
     virtual bool vector() const { return true; }
 
@@ -103,17 +110,19 @@ public:
 
     virtual std::ostream& print( std::ostream& o ) const { std::copy(v_.begin(),v_.end(), std::ostream_iterator<Vector::value_t>(o, " ")); return o; }
 
-    static VarPtr make( const size_t& s, const scalar_t& v = scalar_t() ) { return VarPtr( new Vector(s,v) ); }
-
 protected:
     storage_t v_;
 };
+
+VarPtr vector( const size_t& sz, const scalar_t& v = scalar_t()  ) { return VarPtr( new Vector(sz,v) ); }
+VarPtr vector( const Vector::storage_t& v  ) { return VarPtr( new Vector(v) ); }
 
 //-----------------------------------------------------------------------------
 
 class Add {
 public:
 
+    /// Represents a Add expression
     struct Op : public Exp
     {
         ExpPtr lhs_;
@@ -129,7 +138,7 @@ public:
             bool rhs_vector = rhs_->vector();
 
             if( lhs_scalar && rhs_scalar )
-                return Scalar::make( lhs_->as<Scalar>()->value() + rhs_->as<Scalar>()->value() );
+                return maths::scalar( lhs_->as<Scalar>()->value() + rhs_->as<Scalar>()->value() );
             if( lhs_scalar && rhs_vector )
                 return eval_add_vector_scalar( lhs_, rhs_ );
             if( lhs_vector && rhs_scalar )
@@ -193,48 +202,168 @@ public:
 
 };
 
+// version with stand alone functions
+
+ExpPtr add( ExpPtr l, ExpPtr r ) { return ExpPtr( new Add::Op(l,r) ); }
+ExpPtr add( Exp&   l, ExpPtr r ) { return ExpPtr( new Add::Op(l.self(),r) ); }
+ExpPtr add( ExpPtr l, Exp&   r ) { return ExpPtr( new Add::Op(l,r.self()) ); }
+ExpPtr add( Exp&   l, Exp&   r ) { return ExpPtr( new Add::Op(l.self(),r.self()) ); }
+
+//-----------------------------------------------------------------------------
+
+/// @todo find a way to avoid this duplication of classes
+
+class Prod {
+public:
+
+    /// Represents a Prod expression
+    struct Op : public Exp
+    {
+        ExpPtr lhs_;
+        ExpPtr rhs_;
+
+        Op( ExpPtr lhs, ExpPtr rhs ) : lhs_(lhs), rhs_(rhs) {}
+
+        VarPtr eval()
+        {
+            bool lhs_scalar = lhs_->scalar();
+            bool lhs_vector = lhs_->vector();
+            bool rhs_scalar = rhs_->scalar();
+            bool rhs_vector = rhs_->vector();
+
+            if( lhs_scalar && rhs_scalar )
+                return maths::scalar( lhs_->as<Scalar>()->value() * rhs_->as<Scalar>()->value() );
+            if( lhs_scalar && rhs_vector )
+                return eval_prod_vector_scalar( lhs_, rhs_ );
+            if( lhs_vector && rhs_scalar )
+                return eval_prod_vector_scalar( rhs_, lhs_ );
+            if( lhs_vector && rhs_vector )
+                return eval_prod_vector_vector( rhs_, lhs_ );
+
+            assert( ! lhs_->isVar() || ! rhs_->isVar() ); // either one is not a Var
+
+            /// @todo optimize this dispatch by looking into possible reduction of temporaries
+
+            if( ! lhs_->isVar() )
+                lhs_ = lhs_->eval(); /// @note creates temporary
+
+            if( ! rhs_->isVar() )
+                rhs_ = rhs_->eval(); /// @note creates temporary
+
+            return Op( lhs_, rhs_ ).eval();
+        }
+
+    private:
+
+        VarPtr eval_prod_vector_scalar( ExpPtr& s, ExpPtr& v )
+        {
+            scalar_t lhs = s->as<Scalar>()->value();
+            Vector::storage_t& rhs = v->as<Vector>()->ref_value();
+
+            Vector* res = new Vector( rhs.size() );
+            Vector::storage_t& rv = res->ref_value();
+
+            for( size_t i = 0; i < rv.size(); ++i )
+                rv[i] = lhs * rhs[i];
+
+            return VarPtr(res);
+        }
+
+        VarPtr eval_prod_vector_vector( ExpPtr& v1, ExpPtr& v2 )
+        {
+            Vector::storage_t& lhs = v1->as<Vector>()->ref_value();
+            Vector::storage_t& rhs = v2->as<Vector>()->ref_value();
+
+            assert( lhs.size() == rhs.size() );
+
+            Vector* res = new Vector( rhs.size() );
+            Vector::storage_t& rv = res->ref_value();
+
+            for( size_t i = 0; i < rv.size(); ++i )
+                rv[i] = lhs[i] * rhs[i];
+
+            return VarPtr(res);
+        }
+
+    };
+
+    Prod() {}
+
+    ExpPtr operator() ( ExpPtr l, ExpPtr r ) { return ExpPtr( new Prod::Op(l,r) ); }
+    ExpPtr operator() ( Exp&   l, ExpPtr r ) { return ExpPtr( new Prod::Op(l.self(),r) ); }
+    ExpPtr operator() ( ExpPtr l, Exp&   r ) { return ExpPtr( new Prod::Op(l,r.self()) ); }
+    ExpPtr operator() ( Exp&   l, Exp&   r ) { return ExpPtr( new Prod::Op(l.self(),r.self()) ); }
+
+};
+
+// version with stand alone functions
+
+ExpPtr prod( ExpPtr l, ExpPtr r ) { return ExpPtr( new Prod::Op(l,r) ); }
+ExpPtr prod( Exp&   l, ExpPtr r ) { return ExpPtr( new Prod::Op(l.self(),r) ); }
+ExpPtr prod( ExpPtr l, Exp&   r ) { return ExpPtr( new Prod::Op(l,r.self()) ); }
+ExpPtr prod( Exp&   l, Exp&   r ) { return ExpPtr( new Prod::Op(l.self(),r.self()) ); }
+
+//-----------------------------------------------------------------------------
+
+} // namespace maths
+
 //-----------------------------------------------------------------------------
 
 int main()
 {
-    VarPtr a = Scalar::make( 2. );
-    VarPtr c = Scalar::make( 4. );
+    using namespace maths;
 
-    VarPtr v1 = Vector::make( 10, 5. );
-    VarPtr v2 = Vector::make( 10, 7. );
+    VarPtr a = scalar( 2. );
+    VarPtr c = scalar( 4. );
+
+    VarPtr v1 = maths::vector( 10, 5. );
+    VarPtr v2 = maths::vector( 10, 7. );
 
     std::cout << "a = " << a << std::endl;
     std::cout << "c = " << c << std::endl;
     std::cout << "v1 = " << v1 << std::endl;
     std::cout << "v2 = " << v2 << std::endl;
 
-    // scalar + scalar
+    // scalar + scalar --> 6 = 2 + 4
 
     VarPtr r = Add()( a , c )->eval();
     std::cout << "r = " <<  r << std::endl;
 
-    // scalar + vector
+    // scalar + vector --> 7 = 2 + 5
 
     VarPtr r2 = Add()( a , v1 )->eval();
     std::cout << "r2 = " << r2 << std::endl;
 
-    // vector + scalar
+    // vector + scalar  --> 7 = 5 + 2
 
     std::cout << "r2 = " << Add()( v1 , a )->eval() << std::endl;
 
-    // vector + vector
+    // vector + vector --> 12 = 5 + 7
 
     std::cout << "r3 = " << Add()( *v1, *v2 )->eval() << std::endl;
 
-    // vector + vector
+    // vector + vector --> 10 = 5 + 5
 
-    ExpPtr add = Add()( v1, v1 );                // look, we can store expressions
-    VarPtr r4 =  add->eval();                  // and evaluate them later...
+    ExpPtr addop = Add()( v1, v1 );             // look, we can store expressions
+    VarPtr r4 = addop->eval();                  // and evaluate them later...
     std::cout << "r4 = " << r4 << std::endl;
 
-    // vector + expression
+    // vector + vector --> 12 = 5 + 7
 
-    std::cout << "r5 = " << Add()( Add()( v1, v2 ) , Add()( v1, v2 ) )->eval() << std::endl;
+    Add addx; // or we can store the expression generators and delay generation ( usefull in worker threads )
+    std::cout << "r5 = " << addx( v1, v2 )->eval() << std::endl;
+
+    // vector + expression --> 17 = 5 + ( 5 + 7 )
+
+    std::cout << "r6 = " << add( v1 , add( *v1, v2 ) )->eval() << std::endl;
+
+    // chain v = v1 * ( v1 + v2 ) --> 60 = 5 * ( 5 + 7 )
+
+    std::cout << "r7 = " << prod( v1 , add( *v1, v2 ) )->eval() << std::endl;
+
+    // optimization : v = s * ( v1 + v2 ) --> 24 = 2 * ( 5 + 7 )
+
+    std::cout << "r8 = " << prod( scalar(2.) , add( *v1, v2 ) )->eval() << std::endl;
 
 #if 0
 
