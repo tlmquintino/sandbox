b7e3871a7c219db5a8e4a2b93a41e2a2d9c06c2a
Tiago Quintino <tiago.quintino@ecmwf.int>
2013-12-06 13:25:12 +0000

    better performance fib

--- boost/ycombinator/boost_ycombinator.cc
+++ boost/ycombinator/boost_ycombinator.cc
@@ -14,6 +14,17 @@ int fact( int_2_int_f f, int v )
     return v * f(v -1);
 }
 
+// Y-combinator compatible fibonacci
+// which avoids recursion
+int fib( int_2_int_f f, int v )
+{
+    if( v == 0 ) return 0;
+    if( v == 1 ) return 1;
+    if( v == 2 ) return 1; // optional -- better performance
+    if( v == 3 ) return 2; // optional -- better performance
+    return f(v-1) + f(v-2);
+}
+
 // Y-combinator for the int type
 int_2_int_f ycomb ( boost::function< int ( int_2_int_f, int ) > f )
 {
@@ -25,5 +36,9 @@ int main(int argc,char** argv)
 {
   int_2_int_f factorial = ycomb( fact );
   std::cout << factorial(5) << std::endl;
+
+  int_2_int_f fibonacci = ycomb( fib );
+  std::cout << fibonacci(20) << std::endl;
+
   return 0;
 }
